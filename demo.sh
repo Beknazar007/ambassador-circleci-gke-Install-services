dd51115339ee8fbdd28b1e7ab738ee24f5f9b30f
Bekanzar007 <saitovbeknazar6@gmail.com>
2021-12-21 17:12:17 +0600

    push to new cluster

--- demo.sh
+++ demo.sh
@@ -12,10 +12,10 @@ then
     echo " "
 else
     echo "-----INSTALLING HELM-------"
-    sudo apt-get install wget
+    apt-get install wget
     wget https://get.helm.sh/helm-v3.6.0-linux-amd64.tar.gz
     tar xvf helm-v3.6.0-linux-amd64.tar.gz
-    sudo mv linux-amd64/helm /usr/local/bin
+    mv linux-amd64/helm /usr/local/bin
     rm helm-v3.6.0-linux-amd64.tar.gz
 fi
 
