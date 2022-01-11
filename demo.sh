fc8f5fb6f701ac66cbdbe5ec5f853b4a7f4a6cbf
Bekanzar007 <saitovbeknazar6@gmail.com>
2022-01-11 06:52:32 +0600

    testing 3 with wget,  on 10th of Jan

--- demo.sh
+++ demo.sh
@@ -8,7 +8,7 @@ then
     echo "-------------------------------------------------------------- "
     echo "-------------------------------------------------------------- "
     echo "-----INSTALLING HELM------------------------------------------"
-    apt install wget
+     
     wget https://get.helm.sh/helm-v3.6.0-linux-amd64.tar.gz
     tar xvf helm-v3.6.0-linux-amd64.tar.gz
     mv linux-amd64/helm /usr/local/bin
